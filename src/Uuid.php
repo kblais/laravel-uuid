9ed7c7530d35f5ed9a88a38975a82255c6d6ea69
Muah <muah003@gmail.com>
2016-12-29 12:23:15 +0200

    update rdme and better reformat for the trait

--- src/Uuid.php
+++ src/Uuid.php
@@ -3,9 +3,13 @@
 namespace Kblais\Uuid;
 
 use Ramsey\Uuid\Uuid as RamseyUuid;
+use Ramsey\Uuid\Exception\UnsatisfiedDependencyException;
 
 trait Uuid
 {
+    protected $uuid_version = 4;
+    protected $uuid_string  = '';
+
     public function getIncrementing()
     {
         return false;
@@ -24,6 +28,31 @@ trait Uuid
 
     protected function generateUuid()
     {
-        return RamseyUuid::uuid4()->toString();
+        try {
+            switch ($this->uuid_version) {
+               case 1:
+                    return RamseyUuid::uuid1()->toString();
+                    break;
+
+               case 3:
+                    return RamseyUuid::uuid3(RamseyUuid::NAMESPACE_DNS, $this->uuid_string)->toString();
+                    break;
+
+               case 4:
+                    return RamseyUuid::uuid4()->toString();
+                    break;
+
+               case 5:
+                    return RamseyUuid::uuid5(RamseyUuid::NAMESPACE_DNS, $this->uuid_string)->toString();
+                    break;
+
+               default:
+                    break;
+           }
+        } catch (UnsatisfiedDependencyException $e) {
+            // Some dependency was not met. Either the method cannot be called on a
+            // 32-bit system, or it can, but it relies on Moontoast\Math to be present.
+            return 'Caught exception: '.$e->getMessage()."\n";
+        }
     }
 }
